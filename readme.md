2be2536dd90d8c13baf668aeb7644d0c6c5ea7ba
#Marcus <205938874+0xbatuhan3@users.noreply.github.com>
2025-04-09 23:55:22 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+
+
+
+
+### Orjinal portları kullanıyorsanız ve herhangi bir script ile kurulum yapmadıysanız kullanın ...
+<pre class="notranslate"><code>wget -O sinyordes.sh https://raw.githubusercontent.com/sinyordes/cosmos-port/main/sinyordes.sh && chmod +x sinyordes.sh && ./sinyordes.sh
+</code></pre>
+
+Prox_App Port(Default- 26658)
+Laddr_Port(Default-26657)
+Proof_Laddr Port(Default-6060)
+Laddr2 Port(26656):
+Prometheus Port (Defaul-26660 )
+App.toml Port 1 (Default-9090)
+App.Toml Port 2 (Default-9091)
+Node Port(Default-26657)
+
+
+
+
---
+++ sinyordes.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+echo -e ''
+echo -e '\e[0;36m'
+echo -e '________              _____ _____   __      _____ ________                 '
+echo -e '___  __/_____ __________  /____  | / /_____ __  /____  __ \____  _________ '
+echo -e '__  /   _  _ \__  ___/_  __/__   |/ / _  _ \_  __/__  /_/ /_  / / /__  __ \'
+echo -e '_  /    /  __/_(__  ) / /_  _  /|  /  /  __// /_  _  _, _/ / /_/ / _  / / /'
+echo -e '/_/     \___/ /____/  \__/  /_/ |_/   \___/ \__/  /_/ |_|  \__,_/  /_/ /_/ '
+echo -e '                                                                           '
+echo -e '\e[0m'
+echo -e ''
+
+GREEN="\e[32m"
+NC="\e[0m"
+BINARY=$(uname -m)
+
+#!/bin/bash
+
+sleep 2
+
+echo "Proje Adı: "
+read PROJE
+echo "Prox_App Port(Default- 26658) :"
+read PROXY
+echo "Laddr_Port(Default-26657) :"
+read LADDR
+echo "Proof_Laddr Port(Default-6060) :"
+read PROOF
+echo  "Laddr2 Port(26656):"
+read LADDR2
+echo "Prometheus Port (Defaul-26660 ):"
+read PROM
+DATA=("."$PROJE)
+echo "App.toml Port 1 (Default-9090):"
+read APP1
+echo "App.Toml Port 2 (Default-9091):"
+read APP2
+echo "Node Port(Default-26657):"
+read NODE
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:$PROXY\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:$LADDR\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:$PROOF\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:$LADDR2\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":$PROM\"%" $HOME/$DATA/config/config.toml
+
+sed -i.bak -e "s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:$APP1\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:$APP2\"%" $HOME/$DATA/config/app.toml
+
+sed -i.bak -e "s%^node = \"tcp://localhost:26657\"%node = \"tcp://localhost:$NODE\"%" $HOME/$DATA/config/client.toml
+
+systemctl restart $PROJE"d"
+
+echo " "
+
+echo "--------------------------------"
+echo "İşlem başarılı..."
+echo "--------------------------------"
+sleep 2
+
